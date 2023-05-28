d44cf06c580bb6f1119b6c86c2d9bbdd8319964c
sooto selim <adewaleselim6@gmail.com>
2023-05-28 13:49:19 +0100

    working 6

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 // import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 // import UseThemesSwitcher from "./Hooks/UseThemesSwitcher";
 
@@ -14,17 +15,17 @@ const CustomLink = ({ href, title, className = "" }) => {
   return (
     <Link
       href={href}
-      className={`${className} relative group w-78 h-24 font-sfprodisplay font-normal text-20  leading-24 text-gray-600`}
+      className={`${className} relative group w-78 font-sfprodisplay font-normal text-[15px]  leading-24 text-gray-600 ${
+        asPath === href
+          ? "font-bold text-[15px] leading-24 text-yellow-500"
+          : ""
+      }`}
     >
       {title}
       <span
-        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+        className={`h-[1px] inline-block bg-[#FAC800] absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
          dark:bg-light
-      ${
-        asPath === href
-          ? "w-full font-bold text-20 leading-24 text-yellow-500"
-          : "w-0"
-      }
+      ${asPath === href ? "w-full " : "w-0"}
       `}
       >
         &nbsp;
@@ -44,7 +45,13 @@ const Header = () => {
       <div className="w-full py-1  dark:text-light border-b-[1px] border-transparent">
         <div className="cont font-medium flex items-center justify-between py-4 relative">
           <div>
-            <img src="/images/Group.png" alt="" />
+            <Image
+              src="/images/Vector.svg"
+              width={300}
+              height={300}
+              alt="logo"
+              className="w-20 bg-black"
+            />
           </div>
           {/* toggle */}
           <button
@@ -69,13 +76,28 @@ const Header = () => {
           </button>
 
           {/* Desktop nav */}
-          <div className="w-fulfl justify-between items-center hidden lg:flex">
-            <nav>
-              <CustomLink href="/about" title="About us" className="mx-2" />
+          <div className=" justify-between items-center hidden lg:flex">
+            <nav className=" mr-16">
+              <CustomLink
+                href="/about"
+                title="About us"
+                className="mx-2 font-bold text-[15px] leading-24 text-yellow-500"
+              />
               <CustomLink href="/products" title="Products" className="mx-2" />
               <CustomLink href="/gallery" title="Gallery" className="mx-2" />
               <CustomLink href="/contact" title="Contact us" className="mx-2" />
             </nav>
+            <div className="flex  items-center">
+              <p className=" leading-24 text-gray-600 font-bold text-[16px] mr-2">
+                EN
+              </p>
+              <Image
+                src="/images/VectorLanguage.svg"
+                width="700"
+                height="700"
+                className="w-5"
+              />
+            </div>
           </div>
 
           {/* Mobile nav */}
@@ -84,31 +106,32 @@ const Header = () => {
               initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
               animate={{ scale: 1, opacity: 1 }}
               className={`min-w-[70vw] flex flex-col justify-between items-center fixed top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2
-           bg-dark/90 dark:bg-light/90 dark:text-dark/90 backdrop-blur-md py-32 lg:hidden `}
+           bg-dark/90 backdrop-blur-md py-32 lg:hidden `}
             >
               <nav
                 className="flex flex-col justify-center items-center text-light/75"
                 onClick={() => setisOpen(false)}
               >
                 <CustomLink
-                  href="/"
-                  title="Home"
-                  className="m-2 dark:text-dark/90"
+                  href="about"
+                  title="About us
+"
+                  className="m-2 text-white font-bold text-[15px] leading-24 text-yellow-500"
                 />
                 <CustomLink
-                  href="/about"
-                  title="About"
-                  className="m-2 dark:text-dark/90"
+                  href="/products"
+                  title="Products"
+                  className="m-2 text-white"
                 />
                 <CustomLink
-                  href="/project"
-                  title="Project"
-                  className="m-2 dark:text-dark/90"
+                  href="/gallery"
+                  title="Gallery"
+                  className="m-2 text-white"
                 />
                 <CustomLink
-                  href="/resume"
-                  title="Resume"
-                  className="m-2 dark:text-dark/90"
+                  href="/contact"
+                  title="Contact us"
+                  className="m-2 text-white"
                 />
               </nav>
             </motion.div>
