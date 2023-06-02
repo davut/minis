b27df4cdb4b25c9544d11ebcadc5c03bf74f5a2d
sooto selim <adewaleselim6@gmail.com>
2023-06-02 13:26:43 +0100

    working 38

--- components/Header.js
+++ components/Header.js
@@ -177,8 +177,8 @@ const Header = () => {
           <motion.div
             initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
             animate={{ scale: 1, opacity: 1 }}
-            className={`min-w-[70vw] flex flex-col justify-between items-center fixed top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2
-           bg-dark/90 backdrop-blur-md py-32 lg:hidden `}
+            className={`min-w-[70vw] flex flex-col justify-between items-center fixed top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2
+           bg-dark/90 backdrop-blur-md py-32 lg:hidden z-20`}
           >
             <nav
               className="flex flex-col justify-center items-center text-light/75"
